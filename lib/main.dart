ac11c0cf93fe3fe9f0c7d153b6f52795bc66ccab
JJGOD <jayantgodse13@gmail.com>
2024-11-21 20:09:37 +1100

    Update days left to live based on user input.
    - Currently don't do type conversion.
    - Need to move code to another file.

--- lib/main.dart
+++ lib/main.dart
@@ -12,57 +12,92 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  int timeLeftToLive = 0;
+  // Variables
+  String timeLeftToLive = '';
+  final TextEditingController _ageController = TextEditingController();
+
+  // Methods
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: const Text("Memento Mori"),
-        ),
-        body: Container(
-          color: Colors.black,
-          child: Center(
-            child: Column(
-              children: [
-                const Text(
-                  "You have",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
+      home: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.grey,
+            title: const Text("Memento Mori"),
+          ),
+          body: Container(
+            color: Colors.black,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(90.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "You have",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                    // Users time left to live.
+                    Text(
+                      timeLeftToLive == '' ? "∞" : timeLeftToLive,
+                      style: const TextStyle(color: Colors.white, fontSize: 90),
+                    ),
+                    const Text(
+                      "Days left to live",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                    // Textfield to take user input.
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextField(
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                hintText: "Enter Your Age",
+                                suffixIcon: IconButton(
+                                    onPressed: () {
+                                      _ageController.clear();
+                                    },
+                                    icon: const Icon(Icons.clear)),
+                                border: const OutlineInputBorder()),
+                            controller: _ageController,
+                            keyboardType:
+                                const TextInputType.numberWithOptions(),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: MaterialButton(
+                              padding: const EdgeInsets.all(10),
+                              onPressed: () {
+                                setState(() {
+                                  timeLeftToLive =
+                                      _ageController.text.toString();
+                                  _ageController.clear();
+                                });
+                              },
+                              color: Colors.blue,
+                              child: const Text("Set Age"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  timeLeftToLive == 0 ? "∞" : "$timeLeftToLive",
-                  style: const TextStyle(color: Colors.white, fontSize: 90),
-                ),
-                const Text(
-                  "Days left to live",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if (timeLeftToLive != null) {
-              setState(() {
-                timeLeftToLive++;
-              });
-            } else {
-              setState(() {
-                timeLeftToLive = 1;
-              });
-            }
-          },
-          child: const Icon(Icons.plus_one),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
