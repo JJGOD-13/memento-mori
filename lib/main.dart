39e2e0a4e119c833748bccbf428c7663841dd2e3
JJGOD <jayantgodse13@gmail.com>
2024-12-17 10:05:02 +1100

    Move the logic to 2 different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memento_mori/src/widgets/home_page.dart';
 import 'package:memento_mori/src/widgets/time_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -36,57 +37,56 @@ class _MainAppState extends State<MainApp> {
     final pref = await SharedPreferences.getInstance();
 
     pref.setString('timeLeftToLive', s);
+    setState(() {
+      timeLeftToLive = s;
+    });
+  }
+
+  Future<void> _deletepreferences() async {
+    final pref = await SharedPreferences.getInstance();
+    pref.clear();
+    setState(() {
+      timeLeftToLive = '';
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Builder(builder: (context) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.grey,
-            title: const Text("Memento Mori"),
-          ),
-          body: Container(
-            color: Colors.black,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(90.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center, // Center items vertically
-                  crossAxisAlignment: CrossAxisAlignment.center, // Center items horizontally
-                  children: [
-                    const Text(
-                      "You have",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                      ),
-                    ),
-                    // Users time left to live.
-                    Text(
-                      timeLeftToLive == '' ? "∞" : timeLeftToLive,
-                      style: const TextStyle(color: Colors.white, fontSize: 90),
-                    ),
-                    const Text(
-                      "Days left to live",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                      ),
-                    ),
-                    // Textfield to take user input.
-                    TimePicker(
-                      _ageController: _ageController,
-                      onSubmit: () => _savePreferences(_ageController.text),
-                    )
-                  ],
-                ),
+    if (timeLeftToLive == "") {
+      return MaterialApp(
+        home: Builder(builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.grey,
+              title: const Text("Memento Mori"),
+            ),
+            backgroundColor: Colors.black,
+            body: Center(
+              child: TimePicker(
+                ageController: _ageController,
+                onSubmit: () async {
+                  await _savePreferences(_ageController.text);
+                  _loadPreferences();
+                },
               ),
             ),
-          ),
-        );
-      }),
-    );
+          );
+        }),
+      );
+    } else {
+      return MaterialApp(
+        home: Builder(builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.grey,
+              title: const Text("Memento Mori"),
+            ),
+            body: HomePage(timeLeftToLive: timeLeftToLive),
+            floatingActionButton:
+                FloatingActionButton(onPressed: () => {_deletepreferences()}),
+          );
+        }),
+      );
+    }
   }
 }
--- lib/src/widgets/time_picker.dart
+++ lib/src/widgets/time_picker.dart
@@ -4,26 +4,32 @@ class TimePicker extends StatelessWidget {
   final TextEditingController ageController;
   final VoidCallback onSubmit;
 
-  const TimePicker(
-      {super.key, required this.ageController, required this.onSubmit});
+  const TimePicker({
+    super.key,
+    required this.ageController,
+    required this.onSubmit,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(40.0),
+      padding: const EdgeInsets.all(40.0),
       child: Column(
+        mainAxisSize: MainAxisSize.min, // Ensure the column takes minimum space
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           TextField(
             style: const TextStyle(color: Colors.white),
             decoration: InputDecoration(
-                hintText: "Enter Your Age",
-                suffixIcon: IconButton(
-                    onPressed: () {
-                      ageController.clear();
-                    },
-                    icon: const Icon(Icons.clear)),
-                border: const OutlineInputBorder()),
+              hintText: "Enter Your Age",
+              suffixIcon: IconButton(
+                onPressed: () {
+                  ageController.clear();
+                },
+                icon: const Icon(Icons.clear),
+              ),
+              border: const OutlineInputBorder(),
+            ),
             controller: ageController,
             keyboardType: const TextInputType.numberWithOptions(),
           ),
@@ -33,7 +39,7 @@ class TimePicker extends StatelessWidget {
               padding: const EdgeInsets.all(10),
               onPressed: onSubmit,
               color: Colors.blue,
-              child: const Text("Set Age"),
+              child: const Text("Submit"),
             ),
           ),
         ],
