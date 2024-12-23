122e3f6421f2d5e6d8161ab10234d7f67a44835c
JJGOD <jayantgodse13@gmail.com>
2024-12-23 12:36:50 +1100

    Get the users death day showcase pref and save it to shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:memento_mori/src/utils/enums.dart';
 import 'package:memento_mori/src/widgets/home_page.dart';
 import 'package:memento_mori/src/widgets/time_picker.dart';
 import 'package:memento_mori/src/utils/time_to_live_algo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +19,8 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   // Variables
+  Expectancy _expectancy = Expectancy(0, 0, 0, 0);
+  var _userDisplayPreference = UserDisplayPreferences.days;
   int _timeLeftToLive = 0;
   final int _avgDeathAge = 82;
   final TextEditingController _ageController = TextEditingController();
@@ -25,30 +29,63 @@ class _MainAppState extends State<MainApp> {
   @override
   void initState() {
     super.initState();
-    _loadPreferences();
+    _loadPreferences(_userDisplayPreference);
   }
 
-  Future<void> _loadPreferences() async {
+  /// Load any user data from the shared_preferences stored on device.
+  /// Takes a [UserDisplayPreferences] enum as input and uses it to decide
+  /// which version of the users death day should be returned.
+  ///
+  /// Example:
+  /// _loadPreferences(UserDisplayPreferences.weeks) // Returns users death day in weeks.;
+  Future<void> _loadPreferences(
+      UserDisplayPreferences displayPreference) async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-      _timeLeftToLive = prefs.getInt('timeLeftToLive') ?? 0;
+      // Get the raw json info
+      final expectancyString = prefs.getString("timesLeftToLive");
+
+      // Parse the json if it's not null
+      if (expectancyString != null) {
+        _expectancy = Expectancy.fromJson(jsonDecode(expectancyString));
+      }
+
+      // match the displayPreference to the correct value from the json string.
+      _timeLeftToLive = switch (displayPreference) {
+        UserDisplayPreferences.days => _expectancy.days!,
+        UserDisplayPreferences.weeks => _expectancy.weeks!,
+        UserDisplayPreferences.months => _expectancy.months!,
+        UserDisplayPreferences.years => _expectancy.years!,
+      };
     });
   }
 
-  Future<void> _saveUserAge(int s) async {
+  /// Saves the user's age input and calculates their remaining time to live.
+  ///
+  /// Takes an [age] parameter as an integer and calculates the time left to live
+  /// based on the average death age ([_avgDeathAge]).
+  ///
+  /// The calculated time is stored in SharedPreferences and updates the UI state.
+  ///
+  /// Throws an [Exception] if the timeLeftToLive calculation returns null.
+  ///
+  /// Example:
+  /// await _saveUserInput(25);
+  Future<void> _saveUserInput(int s) async {
     final pref = await SharedPreferences.getInstance();
 
     // Convert to correct death time
     var time = timeLeftToLive(s, _avgDeathAge);
 
-    if (time == null) {
-      throw Exception("Failed to recieve proper map");
+    if (time != null) {
+      pref.setString("timesLeftToLive", jsonEncode(time.toJson()));
+      pref.setInt('timeLeftToLive', time.days!);
+      setState(() {
+        _timeLeftToLive = time.days!;
+      });
+    } else {
+      throw Exception("time was null");
     }
-
-    pref.setInt('timeLeftToLive', time["Days"]!);
-    setState(() {
-      _timeLeftToLive = s;
-    });
   }
 
   Future<void> _deletepreferences() async {
@@ -56,6 +93,7 @@ class _MainAppState extends State<MainApp> {
     pref.clear();
     setState(() {
       _timeLeftToLive = 0;
+      _expectancy.clear();
     });
   }
 
@@ -74,8 +112,14 @@ class _MainAppState extends State<MainApp> {
               child: TimePicker(
                 ageController: _ageController,
                 onSubmit: () async {
-                  await _saveUserAge(int.parse(_ageController.text));
-                  _loadPreferences();
+                  await _saveUserInput(int.parse(_ageController.text));
+                  _loadPreferences(UserDisplayPreferences.days);
+                },
+                onPreferenceChanged: (pref) {
+                  setState(() {
+                    _userDisplayPreference = pref;
+                    _loadPreferences(_userDisplayPreference);
+                  });
                 },
               ),
             ),
