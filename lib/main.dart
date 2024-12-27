dcb4aaad9913505aaf0b8ceddf4be6eab5ebe3f9
JJGOD <jayantgodse13@gmail.com>
2024-12-27 13:13:11 +1100

    reconfigure main to use the provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:memento_mori/src/providers/user_age_provider.dart';
+import 'package:memento_mori/src/providers/user_display_prefs_provider.dart';
 import 'package:memento_mori/src/utils/enums.dart';
 import 'package:memento_mori/src/widgets/home_page.dart';
 import 'package:memento_mori/src/widgets/time_picker.dart';
 import 'package:memento_mori/src/utils/time_to_live_algo.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
@@ -23,69 +26,24 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  // Variables
-  Expectancy _expectancy = Expectancy(0, 0, 0, 0);
-  final _userDisplayPreference = UserDisplayPreferences.days;
-  int _timeLeftToLive = 0;
-
-  // Methods
-  @override
-  void initState() {
-    super.initState();
-    _loadPreferences(_userDisplayPreference);
-  }
-
-  /// Load any user data from the shared_preferences stored on device.
-  /// Takes a [UserDisplayPreferences] enum as input and uses it to decide
-  /// which version of the users death day should be returned.
-  ///
-  /// Example:
-  /// _loadPreferences(UserDisplayPreferences.weeks) // Returns users death day in weeks.;
-  Future<void> _loadPreferences(
-      UserDisplayPreferences displayPreference) async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      // Get the raw json info
-      final expectancyString = prefs.getString("timesLeftToLive");
-
-      // Parse the json if it's not null
-      if (expectancyString != null) {
-        _expectancy = Expectancy.fromJson(jsonDecode(expectancyString));
-      }
-
-      // match the displayPreference to the correct value from the json string.
-      _timeLeftToLive = switch (displayPreference) {
-        UserDisplayPreferences.days => _expectancy.days!,
-        UserDisplayPreferences.weeks => _expectancy.weeks!,
-        UserDisplayPreferences.months => _expectancy.months!,
-        UserDisplayPreferences.years => _expectancy.years!,
-      };
-    });
-  }
-
-  /// Saves the user's age input and calculates their remaining time to live.
-  ///
-  /// Takes an [age] parameter as an integer and calculates the time left to live
-  /// based on the average death age ([_avgDeathAge]).
-  ///
-  /// The calculated time is stored in SharedPreferences and updates the UI state.
-  ///
-  /// Throws an [Exception] if the timeLeftToLive calculation returns null.
-  ///
-  /// Example:
-  /// await _saveUserInput(25);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserAgeProvider()),
+        ChangeNotifierProvider(create: (context) => UserDisplayPrefsProvider()),
+      ],
+      child: MaterialApp(
         theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: Colors.black,
         ),
         routes: {
           MementoRoutes.home: (context) => const HomePage(),
-          MementoRoutes.input: (context) => const TimePicker(),
+          MementoRoutes.input: (context) => TimePicker(),
         },
-        home: _timeLeftToLive == 0 ? HomePage() : TimePicker());
+        initialRoute: MementoRoutes.home,
+      ),
+    );
   }
 }
