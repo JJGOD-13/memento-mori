d2c89cf0753d8d4f24cdaee83e19a1e8e992fe34
JJGOD <jayantgodse13@gmail.com>
2024-12-31 13:20:06 +1100

    Configure slide over drawer
    - Drawer title
    - Drawer options, (Settings and about)
    - spacing and misc

--- lib/src/widgets/memento_drawer.dart
+++ lib/src/widgets/memento_drawer.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:memento_mori/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MementoDrawer extends StatelessWidget {
   const MementoDrawer({super.key});
+  final url = "https://github.com/JJGOD-13/memento-mori";
+
+  Future<void> _launchURL() async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw "COULD NOT LAUNCH $url";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Drawer(
-        backgroundColor: Colors.black,
-        child: const Center(
-          child: Text(
-            "Hello Achal",
-            style: TextStyle(color: Colors.white),
+    return Drawer(
+      backgroundColor: Colors.grey.shade900,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 150,
+            child: DrawerHeader(
+              margin: EdgeInsets.zero,
+              child: Text(
+                "Memento Mori",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-        ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: const Text("Settings"),
+            onTap: () =>
+                Navigator.popAndPushNamed(context, MementoRoutes.input),
+          ),
+          ListTile(
+            leading: Icon(Icons.people),
+            title: const Text("About"),
+            onTap: _launchURL,
+          ),
+        ],
       ),
     );
   }
