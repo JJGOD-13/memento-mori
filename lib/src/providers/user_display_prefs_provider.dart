191d816ad94200e6d774de257552d4e474df7647
achalsingh <asin0142@student.monash.edu>
2025-01-04 03:08:19 +1100

    Implemented the SharedPreferences package to store user input for both age and display

--- lib/src/providers/user_display_prefs_provider.dart
+++ lib/src/providers/user_display_prefs_provider.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:memento_mori/src/utils/enums.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserDisplayPrefsProvider extends ChangeNotifier {
   UserDisplayPreferences userDisplayPreference;
 
   UserDisplayPrefsProvider({
     this.userDisplayPreference = UserDisplayPreferences.days,
-  });
+  }) {
+    loadUserDisplay();
+  }
 
-  void setUserDisplayPref({
-    required UserDisplayPreferences newPref,
-  }) async {
+  /// Load display preference from SharedPreferences
+  Future<void> loadUserDisplay() async{
+    final prefs = await SharedPreferences.getInstance();
+    String? savedPref = prefs.getString('userDisplayPreference');
+    if (savedPref != null) {
+      userDisplayPreference = UserDisplayPreferences.values.firstWhere(
+        (e) => e.toString() == savedPref,
+        orElse: () => UserDisplayPreferences.days,
+      );
+      notifyListeners();
+    }
+  }
+
+  Future<void> setUserDisplayPref({required UserDisplayPreferences newPref}) async{
     userDisplayPreference = newPref;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('userDisplayPreference', newPref.toString());
     notifyListeners();
   }
 }
