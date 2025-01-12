0f5c119b06590aa602c235e5b7b8a65c23bc998b
JJGOD <jayantgodse13@gmail.com>
2025-01-12 19:18:24 +1100

    Function to get the users Expectancy

--- lib/src/providers/user_age_provider.dart
+++ lib/src/providers/user_age_provider.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
+import 'package:memento_mori/src/providers/user_display_prefs_provider.dart';
+import 'package:memento_mori/src/utils/time_to_live_algo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserAgeProvider extends ChangeNotifier {
-  int userAge;
+  int userAge = 0;
+  int genericDeathDay = 80;
 
   UserAgeProvider({
     this.userAge = 0,
@@ -18,10 +23,22 @@ class UserAgeProvider extends ChangeNotifier {
   }
 
   /// Save user's age to SharedPreferences
-  Future<void> setUserAge({required int newAge}) async{
+  Future<void> setUserAge({required int newAge}) async {
     userAge = newAge;
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('userAge', newAge);
     notifyListeners();
+  }
+
+  int getUserExpectancy() {
+    var user_display_pref_prov = UserDisplayPrefsProvider();
+    var user_display_pref = user_display_pref_prov.userDisplayPreference;
+    var age = userAge;
+    try {
+      var time_left_to_live = timeLeftToLive(age, genericDeathDay);
+      return time_left_to_live!.fromDisplayPref(user_display_pref);
+    } catch (e) {
+      return 6969;
+    }
   }
 }
